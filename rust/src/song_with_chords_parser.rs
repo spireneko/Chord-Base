4a1974cf12abcc57dc70fe0e6fe370c8ba8adc0b
spireneko <spireneko@gmail.com>
2025-04-05 10:56:52 +0400

    add song parser

--- rust/src/song_with_chords_parser.rs
+++ rust/src/song_with_chords_parser.rs
@@ -6,89 +6,94 @@ use logos::Logos;
 #[derive(GodotClass)]
 #[class(no_init)]
 struct SongWithChordsParser {
+    #[var(get)]
     chords: PackedStringArray,
-    song_text: GString,
+    #[var(get)]
+    chords_positions: Array<Gd<ChordPosition>>,
+    #[var(get)]
+    song_text: PackedStringArray,
 }
 
 #[godot_api]
 impl SongWithChordsParser {
-    // #[func]
-    // fn create(input: String) -> Gd<Self> {
-    //     let mut lexer = Token::lexer(&input);
-    //     let mut chords_set = HashSet::new();
-    //     let mut song_text = GString::new();
+    #[func]
+    fn create(input: String) -> Gd<Self> {
+        let mut lexer = Token::lexer(&input);
+        let mut chords_set = HashSet::new();
+        let mut song_text = PackedStringArray::new();
+        let mut line_text = String::new();
+        let mut chords_positions = Array::new();
 
-    //     while let Some(token) = lexer.next() {
-    //         match token {
-    //             Err(_) => break,
-    //             Ok(token) => match token {
-    //                 Token::Chord(chord) => {
-    //                     chords_set.insert(chord);
-    //                 }
-    //             },
-    //         }
-    //     }
+        let mut line_num = 0;
+        let mut line_offset = 0;
 
-    //     Gd::from_object(Self {
-    //         chords: PackedStringArray::from(chords_set.into()),
-    //         song_text,
-    //     })
-    // }
+        while let Some(token) = lexer.next() {
+            match token {
+                Err(_) => break,
+                Ok(token) => match token {
+                    Token::Chord(chord) => {
+                        chords_set.insert(chord.clone());
+                        let position = ChordPosition {
+                            chord: chord.into(),
+                            line: line_num,
+                            offset: line_offset,
+                        };
+                        chords_positions.push(&Gd::from_object(position));
+                    }
+                    Token::Text(text) => {
+                        line_offset += text.chars().count() as u32;
+                        line_text.push_str(&text);
+                    }
+                    Token::Newline => {
+                        line_num += 1;
+                        line_offset = 0;
+                        song_text.push(&GString::from(line_text.clone()));
+                        line_text.clear();
+                    }
+                },
+            }
+        }
+
+        if !line_text.is_empty() {
+            song_text.push(&GString::from(line_text));
+        }
+
+        let chords_list: Vec<GString> = chords_set.into_iter().map(|chord| chord.into()).collect();
+
+        Gd::from_object(Self {
+            chords: PackedStringArray::from(chords_list),
+            chords_positions,
+            song_text: song_text.into(),
+        })
+    }
 }
-
-// Я<Am> бегу по вы<E>жженной земле<Am>,
-// Гермошлем зах<Dm>лопнув н<G>а ходу<C>.    <E>
-// М<Am>ой "Фантом" стрел<C>ою белой на<D> распластанно<F>м крыле
-// С р<Am>евом набира<E>ет высоту<Am>.    <E>
 
 #[derive(Logos)]
 enum Token {
     /// Looking for chords inside <> brackets.
-    /// Brakets will be excluded from token.
+    /// Brackets will be excluded from token.
     #[regex(r"<[^>]+>", |lex| {
         let slice = lex.slice();
-        let slice = &slice[1..slice.len()-1];
-        GString::from(slice)
+        slice[1..slice.len()-1].to_string()
     })]
-    Chord(GString),
+    Chord(String),
+
+    /// Looking for newline separators.
+    #[regex(r"\n")]
+    Newline,
 
     /// Looking for any text, that is not chord.
-    #[regex(r"[^<]+", |lex| {
-        let slice = lex.slice();
-        GString::from(slice)
-    })]
-    Text(GString),
+    #[regex(r"[^[<\n]]+", |lex| lex.slice().to_string())]
+    Text(String),
 }
 
-#[cfg(test)]
-mod tests {
-    use std::collections::HashSet;
-
-    use godot::{builtin::GString, meta::ToGodot};
-
-    use super::SongWithChordsParser;
-
-    #[test]
-    fn it_works() {
-        let input = r#"Я<Am> бегу по вы<E>жженной земле<Am>,
-Гермошлем зах<Dm>лопнув н<G>а ходу<C>.    <E>
-М<Am>ой "Фантом" стрел<C>ою белой на<D> распластанно<F>м крыле
-С р<Am>евом набира<E>ет высоту<Am>.    <E>"#;
-
-        let song_text = GString::from(
-            r#"Я бегу по выжженной земле,
-Гермошлем захлопнув на ходу.
-Мой "Фантом" стрелою белой на распластанном крыле
-С ревом набирает высоту."#,
-        );
-        let chords: HashSet<GString> = vec!["Am", "E", "C", "Dm", "G", "D"]
-            .into_iter()
-            .map(|chord| chord.into())
-            .collect();
-
-        // let parser = SongWithChordsParser::create(input.to_string());
-
-        // assert_eq!(parser.bind().song_text, song_text);
-        // assert_eq!(HashSet::from_iter(parser.bind().chords.to_vec()), chords)
-    }
+#[derive(GodotClass)]
+#[class(no_init)]
+struct ChordPosition {
+    #[var(get)]
+    chord: GString,
+    #[var(get)]
+    line: u32,
+    #[var(get)]
+    offset: u32,
 }
